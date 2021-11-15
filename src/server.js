17cdb7c77e5c82904ae577520cba89819facd594
CTW100 <85342844+CTW100@users.noreply.github.com>
2021-11-15 22:56:50 +0900

    3.3
    Responses

--- src/server.js
+++ src/server.js
@@ -4,9 +4,15 @@ const PORT = 4000;
 
 const app = express();
 
-const handleHome = () => console.log('Somebody is trying to go home.');
+const handleHome = (req, res) => {
+  return res.send('I still on going.');
+};
+const handlerLogin = (req, res) => {
+  return res.send('Login here.');
+};
 
 app.get('/', handleHome);
+app.get('/login', handlerLogin);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
