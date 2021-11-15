391787cddec372496a912236bcf5dd352929ede9
CTW100 <85342844+CTW100@users.noreply.github.com>
2021-11-16 03:27:02 +0900

    #4.6
    Planning Routes

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,10 +1,12 @@
 import express from 'express';
 
 const globalRouter = express.Router();
-import { join } from '../controllers/userController';
-import { trending } from '../controllers/videoController';
+import { join, login } from '../controllers/userController';
+import { trending, search } from '../controllers/videoController';
 
 globalRouter.get('/', trending);
 globalRouter.get('/join', join);
+globalRouter.get('/login', login);
+globalRouter.get('/search', search);
 
 export default globalRouter;
