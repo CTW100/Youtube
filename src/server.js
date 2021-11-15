697d8334d876b5ebcbac33a0381bb7f73b41a915
CTW100 <85342844+CTW100@users.noreply.github.com>
2021-11-15 23:06:47 +0900

    #3.5
    Middlewares part One

--- src/server.js
+++ src/server.js
@@ -4,15 +4,15 @@ const PORT = 4000;
 
 const app = express();
 
-const handleHome = (req, res) => {
-  return res.send('I still on going.');
+const gossipMiddleware = (req, res, next) => {
+  console.log(`Someone is going to: ${req.url}`);
+  next();
 };
-const handlerLogin = (req, res) => {
-  return res.send('Login here.');
+const handleHome = (req, res) => {
+  return res.send('I love middlewares');
 };
 
-app.get('/', handleHome);
-app.get('/login', handlerLogin);
+app.get('/', gossipMiddleware, handleHome);
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
