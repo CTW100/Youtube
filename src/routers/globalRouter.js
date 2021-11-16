17ef77fcf1dac64a7ea3f87663682ce6872213d6
CTW100 <85342844+CTW100@users.noreply.github.com>
2021-11-16 15:37:50 +0900

    #5.2
    Partials

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,12 +1,12 @@
-import express from 'express';
+import express from "express";
+import { join, login } from "../controllers/userController";
+import { trending, search } from "../controllers/videoController";
 
 const globalRouter = express.Router();
-import { join, login } from '../controllers/userController';
-import { trending, search } from '../controllers/videoController';
 
-globalRouter.get('/', trending);
-globalRouter.get('/join', join);
-globalRouter.get('/login', login);
-globalRouter.get('/search', search);
+globalRouter.get("/", trending);
+globalRouter.get("/join", join);
+globalRouter.get("/login", login);
+globalRouter.get("/search", search);
 
 export default globalRouter;
