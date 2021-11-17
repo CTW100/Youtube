79e6726a9d640811f61e330b0e6fe83b615c87da
CTW100 <85342844+CTW100@users.noreply.github.com>
2021-11-17 13:19:52 +0900

    #6.0
    Array Database part One

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -14,7 +14,7 @@ export const trending = (req, res) => {
       comments: 2,
       createdAt: '2 minutes ago',
       views: 59,
-      id: 1,
+      id: 2,
     },
     {
       title: 'Third Video',
@@ -22,16 +22,18 @@ export const trending = (req, res) => {
       comments: 2,
       createdAt: '2 minutes ago',
       views: 59,
-      id: 1,
+      id: 3,
     },
   ];
   return res.render('home', { pageTitle: 'Home', videos: videos });
 };
 
-export const see = (req, res) => res.render('watch');
+export const see = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  res.render('watch', { pageTitle: `Watching ${video.title}` });
+};
 export const edit = (req, res) => res.render('edit');
 export const search = (req, res) => res.send('Search');
 export const upload = (req, res) => res.send('Upload');
-export const deleteVideo = (req, res) => {
-  return res.send('Delete Video');
-};
+export const deleteVideo = (req, res) => res.send('Delete Video');
