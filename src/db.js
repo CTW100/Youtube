3e1dd12c37517aaf280158ce4e6ea1fd856f01dc
CTW100 <85342844+CTW100@users.noreply.github.com>
2021-11-17 15:04:38 +0900

    #6.8
    Connecting to Mongo

---
+++ src/db.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb://127.0.0.1:27017/youtube', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+const handlerOpen = () => console.log('Connected to DB');
+const handlerError = (error) => console.log('DB Error', error);
+
+db.on('error', handlerEror);
+db.once('open', handleOpen);
